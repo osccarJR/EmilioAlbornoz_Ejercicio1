a7692913f45bb8a4ff02ed9b60358b1197801a14
Emilio Albornoz <oscaremilioalbornoz@hotmail.com>
2024-04-08 11:11:28 -0500

    actualizacion2

--- WebApplication2/wwwroot/js/site.js
+++ WebApplication2/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Autor: Emilio Albornoz
 
-// Write your JavaScript code.
+// Saludar al usuario
+alert("Soy Emilio este es mi primer comentario.");
